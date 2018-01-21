2a421c4e3c3bc23f190968d6e76b4dfb4dfba2bc
Justin Noor <justin@justinnoor.io>
2018-01-20 21:35:21 -0800

    added ch3_ex3_p85.cpp

---
+++ ch3/ch3_ex3_p85.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+
+//Make a program that does nothing except declare multiple variables with illegal names that screw with the compiler.
+
+int main()
+{
+    int 2x = 5;
+    double $$$s_baby = 100.0;
+    bool false = f;
+    char (a) = a;
+    string my string = "this is my string";
+
+	return 0;
+}
+
