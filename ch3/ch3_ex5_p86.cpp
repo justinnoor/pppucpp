f74e1c750840c1cc13f781a08661973eb342936f
Justin Noor <justin@justinnoor.io>
2018-01-22 05:53:04 -0800

    added ch3_ex5_p86.cpp

---
+++ ch3/ch3_ex5_p86.cpp
@@ -0,0 +1,38 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//This program modifies the previous exercise to include double -
+//variables instead of ints. Notice the changes in the results
+
+int main()
+{
+
+    double val1;
+    double val2;
+    
+    cout << "Please enter your numbers:\n";
+    cin >> val1 >> val2;
+
+    if (val1 < val2)
+        cout << "val1: " << val1 << " is smaller than val2: "
+             << val2 << "\n"
+             << "The difference between val2 and val1 is: "
+             << val2 - val1 << "\n";
+    else
+        cout << "val1: " << val1 << " is larger than val2: "
+             << val2 << "\n"
+             << "The difference between val1 and val2 is: "
+             << val1 - val2 << "\n";
+
+	cout << "The sum of val1 + val2 = " << val1 + val2 << "\n"
+         << "The product of val1 and val1 is: " << val2 * val2 << "\n";
+
+    if (val1 < val2)
+        cout << "The ratio val1:val2 = 1:" << val2 / val1 << "\n";
+    else if (val1 > val2)
+        cout << "The ratio val1:val2 = " << val1 / val2 << ":1\n";
+
+	return 0;
+}
+
