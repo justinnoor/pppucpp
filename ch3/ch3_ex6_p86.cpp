296e8a93f6ab9b795f155401458fcec7709af3ab
Justin Noor <justin@justinnoor.io>
2018-01-25 18:12:16 -0800

    added ch3_ex6_p86.cpp

---
+++ ch3/ch3_ex6_p86.cpp
@@ -0,0 +1,43 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//This program modifies 'ch3_ex5_p86.cpp'. It accepts from - 
+//the user three integers, and outputs them sequentially with - 
+//std::cout. They must be separated by commas (i.e., 1, 2, 3). 
+//Please note this program is quite verbose because arrays have - 
+//not been introduced yet in the book and are , therefore, - 
+//not included . Technically, I already cheated by using the - 
+//logical '&&' and '||' operators, as those are not introduced - 
+//until chapter 4. Please contact me if you have a cleaner way - 
+//of doing this. Basically, I isolated the smallest number to the - 
+//left, and worked from there. Thank you.
+
+int main()
+{
+
+    int val1;
+    int val2;
+    int val3;
+
+    cout << "Please enter your numbers:\n";
+    cin >> val1 >> val2 >> val3;
+
+    if (val1 <= val2 && val1 <= val3 && val2 <= val3)
+        cout << val1 << ", " << val2 << ", " << val3 << "\n";
+    else if (val1 <= val2 && val1 <= val3 && val3 <= val2)
+        cout << val1 << ", " << val3 << ", " << val2 << "\n";
+
+    else if (val2 <= val1 && val2 <= val3 && val1 <= val3)
+        cout << val2 << ", " << val1 << ", " << val3 << "\n";
+    else if (val2 <= val1 && val2 <= val3 && val1 >= val3)
+        cout << val2 << ", " << val3 << ", " << val1 << "\n";
+
+    else if (val3 <= val1 && val3 <= val2 && val1 <= val2)
+        cout << val3 << ", " << val1 << ", " << val2 << "\n";
+    else if (val3 <= val1 && val3 <= val2 && val1 >= val2)
+        cout << val3 << ", " << val2 << ", " << val1 << "\n";
+
+	return 0;
+}
+
