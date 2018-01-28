fb53726a6d041d7116e230528247dc6baa1c5cb7
Justin Noor <justin@justinnoor.io>
2018-01-27 22:41:22 -0800

    added ch3_ex7_p86.cpp

---
+++ ch3/ch3_ex7_p86.cpp
@@ -0,0 +1,36 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//This program modifies 'ch3_ex6_p86.cpp'. It accepts strings instead - 
+//of ints from the user, and outputs them sequentially according to - 
+//length, using std::cout.
+
+int main()
+{
+
+    string val1;
+    string val2;
+    string val3;
+
+    cout << "Please enter three words:\n";
+    cin >> val1 >> val2 >> val3;
+
+    if (val1 <= val2 && val1 <= val3 && val2 <= val3)
+        cout << val1 << ", " << val2 << ", " << val3 << "\n";
+    else if (val1 <= val2 && val1 <= val3 && val3 <= val2)
+        cout << val1 << ", " << val3 << ", " << val2 << "\n";
+
+    else if (val2 <= val1 && val2 <= val3 && val1 <= val3)
+        cout << val2 << ", " << val1 << ", " << val3 << "\n";
+    else if (val2 <= val1 && val2 <= val3 && val1 >= val3)
+        cout << val2 << ", " << val3 << ", " << val1 << "\n";
+
+    else if (val3 <= val1 && val3 <= val2 && val1 <= val2)
+        cout << val3 << ", " << val1 << ", " << val2 << "\n";
+    else if (val3 <= val1 && val3 <= val2 && val1 >= val2)
+        cout << val3 << ", " << val2 << ", " << val1 << "\n";
+
+	return 0;
+}
+
