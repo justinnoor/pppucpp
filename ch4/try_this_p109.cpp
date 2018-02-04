4cdc9751d342fa9a6ebc3964fd1e7a715e0cca79
Justin Noor <justin@justinnoor.io>
2018-02-04 08:05:17 -0800

    added try_this_p109.cpp

---
+++ ch4/try_this_p109.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	constexpr double yen = 104.60;
+	constexpr double kroner = 5.66;
+	constexpr double pound = .67;
+	constexpr double usd = 1.00;
+	 
+	char currency;
+	double amount;
+	double conversion;
+
+	cout << "Welcome to the USD currency convertor!\n\n";
+	cout << "Enter 'y' for yen, 'k' for kroner, or 'p' for pounds? \n\n";
+	cin >> currency;
+
+	cout << "Enter the amount you wish to convert: \n\n";
+    cin >> amount;
+
+	switch(currency) {
+        case 'y':
+		    conversion = amount / yen;
+            cout << amount << " yen = " << conversion << " USD\n";
+            break;
+	    case 'k':
+		    conversion = amount / kroner;
+            cout << amount << " kroner = " << conversion << " USD\n";
+            break;
+	    case 'p':
+		    conversion = amount / pound;
+            if(amount == 1)
+                cout << amount << " pound = " << conversion << " USD\n";
+            else
+                cout << amount << " pounds = " << conversion << " USD\n";
+            break;
+	    default:
+		    cout << "Sorry, I am not familiar with that currency.\n\n";
+            break;
+    }
+
+	return 0;
+}
+
