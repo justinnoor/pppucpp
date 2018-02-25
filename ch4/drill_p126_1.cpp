c22743ca67a42ed0295d43901047bf439c419736
Justin Noor <justin@justinnoor.io>
2018-02-25 06:00:37 -0800

    added revised drill_p126_1.cpp

---
+++ ch4/drill_p126_1.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//This is the base program for the drill on p.126.
+
+//Write a while loop that reads in two ints and prints them.
+//Exit the program with the '|' character.
+
+int main()
+{
+    int first;
+    int second;
+    char quit = '|';
+
+    cout << "Enter two integers. Enter '|' to exit  \n";
+
+    while(cin >> first >> second) {
+        if(first == quit)
+            break;
+     
+        else if(second == quit)
+            break;
+
+		cout << "You entered: " << first << " & " << second << '\n';
+    }
+ 
+	return 0;
+}
+
