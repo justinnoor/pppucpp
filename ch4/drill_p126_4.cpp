88e10cfaa7045844c05f5e945ddc62be6274b00c
Justin Noor <justin@justinnoor.io>
2018-03-03 03:57:29 -0800

    added drill_p126_4.cpp

---
+++ ch4/drill_p126_4.cpp
@@ -0,0 +1,38 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//Chapter 4 drill pg.126 step 3:
+//Augment step 2 so that it writes "The numbers are equal" - 
+//if (only) they are equal.
+//Exit the program with the terminating '|' character.
+
+
+
+int main()
+{
+    double first;
+    double second;
+    char quit = '|';
+
+    cout << "Enter two integers. Enter '|' to exit  \n";
+
+    while(cin >> first >> second) {
+        if(first == quit)
+            break;
+        else if(second == quit)
+            break;
+
+        if(first < second)
+            cout << "The smaller value is: " << first
+                 << ", and the larger value is: " << second << '\n';
+        else if(first > second)
+            cout << "The larger value is: " << first
+                 << ", and the smaller value is: " << second << '\n';
+        else if(first == second)
+            cout << "The numbers are equal\n";
+    }
+ 
+	return 0;
+}
+
