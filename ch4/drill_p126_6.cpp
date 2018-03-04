54843dd0f434d43e0697a9e12a7257032e972738
Justin Noor <justin@justinnoor.io>
2018-03-03 20:32:25 -0800

    revised drill_p126_6.cpp

--- ch4/drill_p126_6.cpp
+++ ch4/drill_p126_6.cpp
@@ -4,17 +4,16 @@ using namespace std;
 
 
 //Chapter 4 drill pg.126 step 6:
-//See instructions on pg.126, they are rather lenghty.
+//See instructions on pg.126, they are quite lenghty.
 //Since the instructions specify reading in only one double at a time, - 
-//I move 'var_1' to 'var_2' at the end of the loop.
-//Also, I initialize 'var_2' with zero so that there is a double in - 
-//memory when the user provides their first entry.
-//Let me know how I can improve this!
+//I assign 'var_1' to 'var_2' at the end of the loop to track the values.
+//Also, I initialize 'var_2' with zero (before the loop) so that there - 
+//is something in memory when the user provides the very first entry.
+//As always, let me know how I can improve this!
 
 
 int main()
 {
-    double entry;
     double var_1;
     double var_2 = 0;
 
@@ -22,15 +21,12 @@ int main()
 
     cout << "Enter an integer. Enter '|' to exit  \n";
 
-    while(cin >> entry) {
-
-        var_1 = entry;
-
-        if(entry == quit)
+    while(cin >> var_1) {
+        if(var_1 == quit)
             break; 
 
         if(var_2 == 0)
-            cout << var_1 << " is the largest value so far\n";
+            cout << var_1 << " is the largest, and the only, value so far\n";
         else if(var_1 < var_2)
             cout << var_1 << " is the smallest value so far\n"
                  << var_2 << " is the largest value so far\n";
@@ -38,10 +34,10 @@ int main()
             cout << var_1 << " is the largest value so far\n"
                  << var_2 << " is the smallest value so far\n";
         else if(var_1 == var_2)
-            cout << "Values: " << var_1 << " & " << var_2 << " are equal\n";
+            cout << var_1 << " & " << var_2 << " are equal\n";
 
         if(var_1 - var_2  >= -.01 && var_1 - var_2 <= .01 && var_1 - var_2 != 0)
-            cout << "The numbers: " << var_1 << " & " << var_2 << " are almost equal\n";
+            cout << var_1 << " & " << var_2 << " are almost equal\n";
 
         var_2 = var_1;
     }
