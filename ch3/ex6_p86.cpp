83f7231a428017f8fdc6e3b417df6b1763352a28
Justin Noor <justin@justinnoor.io>
2018-03-09 15:58:41 -0800

    cleanup

--- ch3/ex6_p86.cpp
+++ ch3/ex6_p86.cpp
@@ -2,40 +2,37 @@
 
 using namespace std;
 
-//This program modifies 'ch3_ex5_p86.cpp'. It accepts from the user - 
-//three integers, and outputs them sequentially with std::cout.
-//They must be separated by commas (i.e., 1, 2, 3). Please note this - 
-//program is quite verbose because arrays have not been introduced - 
-//yet in the book and are , therefore, not included . Technically, I - 
-//already cheated by using the logical '&&' operator, as that is not - 
-//introduced until chapter 4. Please contact me if you have a cleaner - 
-//way of doing this. Basically (and obviously), I isolated the smallest - 
-//value, moved it to the left, and worked from there. Thank you.
+//Write a program that prompts the user for three integers.
+//Output them sequentially with std::cout.
+//Separate them with commas (i.e., 1, 2, 3).
+
+//Please note: arrays are not introduced yet and are, therefore, not included.
+
+//As always, let me know how I can clean this up!
 
 int main()
 {
+    int val_1;
+    int val_2;
+    int val_3;
 
-    int val1;
-    int val2;
-    int val3;
+    cout << "Please enter three numbers:\n";
+    cin >> val_1 >> val_2 >> val_3;
 
-    cout << "Please enter your numbers:\n";
-    cin >> val1 >> val2 >> val3;
+    if(val_1 <= val_2 && val_1 <= val_3 && val_2 <= val_3)
+        cout << val_1 << ", " << val_2 << ", " << val_3 << '\n';
+    else if(val_1 <= val_2 && val_1 <= val_3 && val_3 <= val_2)
+        cout << val_1 << ", " << val_3 << ", " << val_2 << '\n';
 
-    if (val1 <= val2 && val1 <= val3 && val2 <= val3)
-        cout << val1 << ", " << val2 << ", " << val3 << "\n";
-    else if (val1 <= val2 && val1 <= val3 && val3 <= val2)
-        cout << val1 << ", " << val3 << ", " << val2 << "\n";
+    else if(val_2 <= val_1 && val_2 <= val_3 && val_1 <= val_3)
+        cout << val_2 << ", " << val_1 << ", " << val_3 << '\n';
+    else if(val_2 <= val_1 && val_2 <= val_3 && val_1 >= val_3)
+        cout << val_2 << ", " << val_3 << ", " << val_1 << '\n';
 
-    else if (val2 <= val1 && val2 <= val3 && val1 <= val3)
-        cout << val2 << ", " << val1 << ", " << val3 << "\n";
-    else if (val2 <= val1 && val2 <= val3 && val1 >= val3)
-        cout << val2 << ", " << val3 << ", " << val1 << "\n";
-
-    else if (val3 <= val1 && val3 <= val2 && val1 <= val2)
-        cout << val3 << ", " << val1 << ", " << val2 << "\n";
-    else if (val3 <= val1 && val3 <= val2 && val1 >= val2)
-        cout << val3 << ", " << val2 << ", " << val1 << "\n";
+    else if(val_3 <= val_1 && val_3 <= val_2 && val_1 <= val_2)
+        cout << val_3 << ", " << val_1 << ", " << val_2 << "\n";
+    else if(val_3 <= val_1 && val_3 <= val_2 && val_1 >= val_2)
+        cout << val_3 << ", " << val_2 << ", " << val_1 << "\n";
 
 	return 0;
 }
