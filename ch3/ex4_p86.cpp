62bb35ffbc7959c815a4ddca3a999ffb9a019369
Justin Noor <justin@justinnoor.io>
2018-03-09 15:30:45 -0800

    modified & cleaned up file

--- ch3/ex4_p86.cpp
+++ ch3/ex4_p86.cpp
@@ -2,33 +2,43 @@
 
 using namespace std;
 
+//Chapter 3 Ex.4 Pg.86
+//Write a program that prompts the user for two integer values.
+//Store them in val_1 and val_2. Determine and print out the -
+//smaller, larger, sum, difference, product, and ratio of these -
+//values.
+
 int main()
 {
-
-    int val1;
-    int val2;
+    int val_1;
+    int val_2;
     
     cout << "Please enter your numbers:\n";
-    cin >> val1 >> val2;
+    cin >> val_1 >> val_2;
 
-    if (val1 < val2)
-        cout << "val1: " << val1 << " is smaller than val2: "
-             << val2 << "\n"
-             << "The difference between val2 and val1 is: "
-             << val2 - val1 << "\n";
-    else
-        cout << "val1: " << val1 << " is larger than val2: "
-             << val2 << "\n"
-             << "The difference between val1 and val2 is: "
-             << val1 - val2 << "\n";
+    if(val_1 == val_2)
+        cout << val_1 << " and " << val_2 << " are equal\n"
+             << "The difference between " << val_1 << " - " << val_2
+             << " = 0\n";
 
-	cout << "The sum of val1 + val2 = " << val1 + val2 << "\n"
-         << "The product of val1 and val1 is: " << val2 * val2 << "\n";
+    else if(val_1 < val_2)
+        cout << val_1 << " is smaller than  " << val_2 << '\n'
+             << "The difference between " << val_1 << " - " << val_2
+             << " = " << val_1 - val_2 << '\n';
+    else if(val_1 > val_2)
+        cout << val_1 << " is larger than " << val_2 << '\n'
+             << "The difference between " << val_1 << " - " << val_2
+             << " = " << val_1 - val_2 << '\n';
 
-    if (val1 < val2)
-        cout << "The ratio val1:val2 = 1:" << val2 / val1 << "\n";
-    else if (val1 > val2)
-        cout << "The ratio val1:val2 = " << val1 / val2 << ":1\n";
+	cout << "The sum of " << val_1 << " + " << val_2 << " = " << val_1 + val_2 << '\n'
+         << "The product of " << val_1 << " * " << val_2 << " = " << val_1 * val_2 << '\n';
+
+    if(val_1 < val_2)
+        cout << "The ratio " << val_1 << ":" << val_2 << " = 1:"
+             << val_2 / val_1 << '\n';
+    else if(val_1 > val_2)
+        cout << "The ratio " << val_1 << ":" << val_2 << " = " 
+             << val_1 / val_2 << ":1\n";
 
 	return 0;
 }
