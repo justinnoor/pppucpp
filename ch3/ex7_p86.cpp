28893353e8af0e8c04f2e974429061419e48d56f
Justin Noor <justin@justinnoor.io>
2018-03-09 21:02:36 -0800

    cleanup

--- ch3/ex7_p86.cpp
+++ ch3/ex7_p86.cpp
@@ -2,33 +2,34 @@
 
 using namespace std;
 
-//This program modifies 'ch3_ex6_p86.cpp', using string values -
-//inteads of ints.
+//Chapter 3 Ex.7 Pg.86
+//Modify the previous program to use string values. If the user enters - 
+//Steinbeck, Hemingway, Fitzgerald, the output should be Fitzgerald, - 
+//Hemingway, Steinbeck.
 
 int main()
 {
-
-    string val1;
-    string val2;
-    string val3;
+    string val_1;
+    string val_2;
+    string val_3;
 
     cout << "Please enter three words:\n";
-    cin >> val1 >> val2 >> val3;
+    cin >> val_1 >> val_2 >> val_3;
 
-    if (val1 <= val2 && val1 <= val3 && val2 <= val3)
-        cout << val1 << ", " << val2 << ", " << val3 << "\n";
-    else if (val1 <= val2 && val1 <= val3 && val3 <= val2)
-        cout << val1 << ", " << val3 << ", " << val2 << "\n";
+    if(val_1 <= val_2 && val_1 <= val_3 && val_2 <= val_3)
+        cout << val_1 << ", " << val_2 << ", " << val_3 << '\n';
+    else if(val_1 <= val_2 && val_1 <= val_3 && val_3 <= val_2)
+        cout << val_1 << ", " << val_3 << ", " << val_2 << '\n';
 
-    else if (val2 <= val1 && val2 <= val3 && val1 <= val3)
-        cout << val2 << ", " << val1 << ", " << val3 << "\n";
-    else if (val2 <= val1 && val2 <= val3 && val1 >= val3)
-        cout << val2 << ", " << val3 << ", " << val1 << "\n";
+    else if(val_2 <= val_1 && val_2 <= val_3 && val_1 <= val_3)
+        cout << val_2 << ", " << val_1 << ", " << val_3 << '\n';
+    else if(val_2 <= val_1 && val_2 <= val_3 && val_1 >= val_3)
+        cout << val_2 << ", " << val_3 << ", " << val_1 << '\n';
 
-    else if (val3 <= val1 && val3 <= val2 && val1 <= val2)
-        cout << val3 << ", " << val1 << ", " << val2 << "\n";
-    else if (val3 <= val1 && val3 <= val2 && val1 >= val2)
-        cout << val3 << ", " << val2 << ", " << val1 << "\n";
+    else if(val_3 <= val_1 && val_3 <= val_2 && val_1 <= val_2)
+        cout << val_3 << ", " << val_1 << ", " << val_2 << '\n';
+    else if(val_3 <= val_1 && val_3 <= val_2 && val_1 >= val_2)
+        cout << val_3 << ", " << val_2 << ", " << val_1 << '\n';
 
 	return 0;
 }
