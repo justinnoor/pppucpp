fdd148f5b6b2269f7a0794947556ee343b30d7b3
Justin Noor <justin@justinnoor.io>
2018-03-10 11:57:22 -0800

    cleanup and separate steps

--- ch3/drill_1_p83.cpp
+++ ch3/drill_1_p83.cpp
@@ -1,44 +1,18 @@
 #include "std_lib_facilities.h"
 
 //Chapter 3, Drill 1, Page 83
+//This drill will create a simple letter template based on user input.
+//Modify the code from pg.60 so that it addresses a person that - 
+//you wish to write to. Make any other modifications accordingly.
 
 int main()
 {
-    cout << "Enter the name of a friend you want to write to:\n";
     string first_name;
+
+    cout << "Enter the name of the person you want to write to:\n";
     cin >> first_name;
-    cout << "Now enter the name of another friend:\n";
-    string second_friend;
-    cin >> second_friend;
-    cout << "Enter 'f' or 'm' to specify the gender of the second friend:\n";
-    char friend_sex;
-    cin >> friend_sex;
-    string friend_inquiry;
 
-    if (friend_sex == 'f')
-        friend_inquiry = "her";
-    else
-        friend_inquiry = "him";
-    
-    cout << "Enter the the age of the first friend:\n";
-    int age;
-    cin >> age;
-    string age_comment;
+    cout << "Dear " << first_name << ", \n";
 
-    cout << "\nDear " << first_name << ",\n\n"
-         << "\tI hope this message finds you well.\n"
-         << "Have you seen " << second_friend << " lately?\n"
-         << "If you see " << friend_inquiry << ", please ask "
-         << friend_inquiry << " to call me.\n"
-         << "I hear you are now " << age << " years old!\n";
-
-         if (age < 12)
-             cout << "Next year you will be " << ++age << "!\n";
-         else if (age == 17)
-             cout << "Next year you'll be able to vote!\n";
-         else if (age > 70)
-             cout << "I hope you're enjoying retirement!\n";
-
-    cout << "\nYours sincerely, \n\n"
-         << "Justin Noor\n\n";
+    return 0;
 }
