ea4a2891d828f6071b0645a9146681a30e597677
Justin Noor <justin@justinnoor.io>
2018-03-10 11:39:18 -0800

    added ex11_p86.cpp

--- ch3/ex11_p86.cpp
+++ ch3/ex11_p86.cpp
@@ -2,58 +2,73 @@
 
 using namespace std;
 
-//Write a program that prompts the user to enter the amount of -
-//coins they have. Print the amounts individually. Account for -
-//singular and plural values, i.e., 2 dimes verses 1 dime. Report -
-//the sum as a float with a dollar sign, i.e., $5.68. 
+//Chapter 3 Ex.11 Pg.86
+//Write a program that prompts the user to enter the number of - 
+//pennies, nickels, dimes, quarters, half-dollars, and one-dollar - 
+//coins they have. Query the user, and print these values separately.
+//Print the combined total in dollars and cents, i.e., $1.57.
+//All values must be grammatically correct, i.e.,"one dime", not "one dimes".
 
 int main()
 {
-    int pennies;
-    int nickels;
-    int dimes;
-    int quarters;
-    int half_dollars;
+    double pennies;
+    double nickels;
+    double dimes;
+    double quarters;
+    double half_dollars;
+    double dollar_coins;
 
-    cout << "How many pennies do you have?\n";
+	cout << "\nWelcome to the C++ change calculator!\n\n";
+
+    cout << "Enter the number of pennies you have:\n";
     cin >> pennies;
-    cout << "How many nickels do you have?\n";
+    cout << "Enter the number of nickels you have:\n";
     cin >> nickels;
-    cout << "How many dimes do you have?\n";
+    cout << "Enter the number of dimes you have:\n";
     cin >> dimes;
-    cout << "How many quarters do you have?\n";
+    cout << "Enter the number of quarters you have:\n";
     cin >> quarters;
-    cout << "How many half-dollars do you have?\n";
+    cout << "Enter the number of half-dollars you have:\n";
     cin >> half_dollars;
+    cout << "Enter the number of dollar-coins you have:\n";
+    cin >> dollar_coins;
 
-    if (pennies == 1)
-       cout << "You have " << pennies << " penny\n";
+    cout << "\nTotals:\n";
+
+    if(pennies == 1)
+        cout << "You have " << pennies << " penny\n";
+    else 
+        cout << "You have " << pennies << " pennies\n";
+    if(nickels == 1)
+        cout << "You have " << nickels << " nickel\n";
     else
-       cout << "You have " << pennies << " pennies\n";
-
-    if (nickels == 1)
-       cout << "You have " << nickels << " nickel\n";
+        cout << "You have " << nickels << " nickels\n";
+    if(dimes == 1)
+        cout << "You have " << dimes << " dime\n";
     else
-       cout << "You have " << nickels << " nickles\n";
-
-    if (dimes == 1)
-       cout << "You have " << dimes << " dime\n";
+        cout << "You have " << dimes  << " dimes\n";
+    if(quarters == 1)
+        cout << "You have " << quarters << " quarter\n";
     else
-       cout << "You have " << dimes << " dimes\n";
-
-    if (quarters == 1)
-       cout << "You have " << quarters << " quarter\n";
+        cout << "You have " << quarters << " quarters\n";
+    if(half_dollars == 1)
+        cout << "You have " << half_dollars << " half-dollar\n";
     else
-       cout << "You have " << quarters << " quarters\n";
-
-    if (half_dollars == 1)
-       cout << "You have " << half_dollars << " half-dollar\n";
+        cout << "You have " << half_dollars << " half-dollars\n";
+    if(dollar_coins == 1)
+        cout << "You have " << dollar_coins << " dollar-coin\n";
     else
-       cout << "You have " << half_dollars << " half-dollars\n";
+        cout << "You have " << dollar_coins << " dollar-coins\n\n";
 
-    cout << "The value of all of your coins is $"
-         << pennies * .01 + nickels * .05 + dimes * .10 + quarters * .25 + half_dollars *.50
-         << "\n";
+    pennies = pennies / 100;
+    nickels = nickels / 20;
+    dimes = dimes / 10;
+    quarters = quarters / 4;
+    half_dollars = half_dollars / 2;
+
+    cout << "\nYou have a combined total of: $"
+         << pennies + nickels + dimes + quarters + half_dollars + dollar_coins
+         << '\n' << '\n';
 
 	return 0;
 }
