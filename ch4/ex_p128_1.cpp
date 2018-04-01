eba67d55a6eb019dc3506f22dfe94dfd0d9aaa11
Justin Noor <justin@justinnoor.io>
2018-04-01 15:58:10 -0700

    modified ex_p128_1.cpp

--- ch4/ex_p128_1.cpp
+++ ch4/ex_p128_1.cpp
@@ -1,9 +1,12 @@
+//Chapter 4, Exercise 1, Page 128
+//===============================
+
+/*Fix program 4.6.3 (p.120) so that it prints a median number. A median number is defined as one that is "exactly in the middle of a sequence of numbers". That means an element may need to be added if the vector has an even number of elements.*/
+
+
 #include "std_lib_facilities.h"
 
 using namespace std;
-
-//Fix program 4.6.3 (p.120) so that it prints a median number. A median number is defined as one that is "exactly in the middle of a sequence of numbers". That means elements may need to be added to the vector if the size of the vector is an even number.
-
 
 int main()
 {
@@ -17,10 +20,13 @@ int main()
     sort(temps);
 
     if(temps.size() % 2 == 0)
-        temps.push_back(temps.size() + 1);
+        temps.push_back(temps[temps.size() -1] - 1);
+        /*The above line "should" add a new element that is 1 less than the largest              element. This will fulfill the definition of "median", and stay clear of the             largest element, or else it may get in the way on the next exercise :).*/
 
-    cout << "The numbers you entered are:\n";
-    
+    sort(temps);
+    //Of course now the elements need to be re-sorted. You knew that.
+
+    cout << "Your numbers are:\n";
     for(double x : temps)
         cout << x << '\n';
 
