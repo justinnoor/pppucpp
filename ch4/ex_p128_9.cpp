d4825822877b74cde4bfc244d5bb8d43b4211181
Justin Noor <justin@justinnoor.io>
2018-04-16 11:03:59 -0700

    ex_p128_9.cpp

--- ch4/ex_p128_9.cpp
+++ ch4/ex_p128_9.cpp
@@ -1,7 +1,7 @@
 //Chapter 4, Page 128, Exercise 9
 //===============================
 
-/*All this program does is try to calculate the amounts of rice grains that the inventor asked for in the previous program. That means simply printing the iterations, nothing fancy. It quickly becomes obvious that after the 6th iteration, which attempts to square something like 65500, the product is too large for the int object. The remaining iterations produce zeros. Just run the program and watch.*/
+/*All this program does is try to calculate the amounts of rice grains that the inventor asked for in the previous program. That means simply printing the iterations, nothing fancy. It quickly becomes obvious that after the 6th iteration, which attempts to square something like 65500, the product is too large for the int object. The reamining iterations produce zeros. This program also experiments with a double, just to see what happens. What is the largest number of squares for which the numbers of grains can be calculated with an int object? How about for a double?.*/
 
 
 #include "std_lib_facilities.h"
@@ -10,24 +10,55 @@ using namespace std;
 
 int main()
 {
-    int chess_square = 0;
-    int grains_per_square = 2;
+    int chess_square_int = 0;
+    int grains_per_square_int = 2;
+    double chess_square_double = 0;
+    double grains_per_square_double = 2;
 
 
-    //Print out square 1 separately
-    cout << "square 1\n"
-         << "grains_per_square = 1\n";
+    //Iterate over the chess board using an int, starting at square 2
+    //===============================================================
+    cout << "An int object produces:\n";
 
-    //Iterate over the chess board, starting at square 2
-    //==================================================
+    //Print square 1 separately
+    cout << "square 1\t" << "grains_per_square = 1\n";
+
 	for(int i = 2; i <= 64; ++i)
     {
-	    chess_square = i;
-        grains_per_square = grains_per_square * grains_per_square;
+	    chess_square_int = i;
+        grains_per_square_int = grains_per_square_int * grains_per_square_int;
 
-        cout << "square " << chess_square << '\n';
-        cout << "grains_per_square = " << grains_per_square << '\n';
+        cout << "square " << chess_square_int << '\t';
+        cout << "grains_per_square = " << grains_per_square_int << '\n';
+
+        if(grains_per_square_int == 65536)
+            break;
     }
+
+
+    //Iterate over the chess board using a double, starting at square 2
+    //=================================================================
+    cout << "A double object produces:\n";
+
+    //Print square 1 separately
+    cout << "square 1\t" << "grains_per_square = 1\n";
+
+	for(int i = 2; i <= 64; ++i)
+    {
+	    chess_square_double = i;
+        grains_per_square_double = 
+            grains_per_square_double * grains_per_square_double;
+
+        cout << "square " << chess_square_double << '\t';
+        cout << "grains_per_square = " << grains_per_square_double << '\n';
+
+		if(grains_per_square_double > 1.34078e+154)
+			break;
+    }
+
+    cout << "The greatest number of squares: \n"
+         << "For an int: " << chess_square_int << '\n'
+         << "For a double: " << chess_square_double << '\n';
 
 	return 0;
 }
