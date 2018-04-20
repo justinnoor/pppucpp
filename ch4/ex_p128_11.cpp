daa01524f08b222c10eb52ffc7fa3dc095cc3243
Justin Noor <justin@justinnoor.io>
2018-04-20 11:06:15 -0700

    re-committed ex_p128.11.cpp

--- ch4/ex_p128_11.cpp
+++ ch4/ex_p128_11.cpp
@@ -1,4 +1,4 @@
-/*This program extracts all of the prime numbers between 1 and 100. Interestingly, the instructions are a bit fuzzy. They seem to imply that the prime numbers should be tested against a pre-existing vector of prime numbers. However, the insructions clearly imply that this is an optional method, therefore, this exercise can be solved according to the student's preferred method.*/.
+/*This program extracts all of the prime numbers between 1 and 100. Extract the numbers from a loop, store them in a vector, and print them to the screen. Interestingly, the instructions for this exercise are a bit blurry. They seem to imply that the prime numbers should be tested against another pre-existing vector of prime numbers. However, the insructions clearly imply that this is optional, therefore, the student can solve the exercise according to their preferred method.*/
 
 
 #include "std_lib_facilities.h"
@@ -23,7 +23,14 @@ int main()
     for(int i = 2; i <= 100; ++i)
     {
         if(is_prime(i))
-            cout << i << '\n';
+            primes.push_back(i);
     }
+
+    cout << "The prime numbers between 1 and 100 are:\n";
+
+    for(int x : primes)
+        cout << x << '\n';
+
+    return 0;
 }
 
