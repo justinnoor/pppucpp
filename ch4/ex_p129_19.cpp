0e87c7b3e3bff0bf115cce83b75b6bd8b1fa77d9
Justin Noor <justin@justinnoor.io>
2018-04-27 15:06:43 -0700

    ex_p129_19.cpp

--- ch4/ex_p129_19.cpp
+++ ch4/ex_p129_19.cpp
@@ -1,6 +1,6 @@
 //Chapter 4, Page 129, Exercise 19
 //================================
-/*Write a program that takes a name and an age, i.e. Inayah 25. The name is a string, and the number is an int, each stored in a separate vector. The string and the int must have the same index number, i.e. vect_str[0] == "Inayah", and vect_int[0] == 25. Terminate the input with 'NoName 0'. Do not allow duplicate names to be read in. Terminate the input with an error message if duplicate names are read. At the end of the program, print all of the names to stdout, one per line (i.e. Inayah,25).*/
+/*Write a program that takes a name and an age, i.e. Inayah 25. The name is a string, and the number is an int. Store the names in vector 'names', and the ints in vector 'scores'. The string and the int must have the same index number, i.e. vect_str[0] == "Inayah", and vect_int[0] == 25. Terminate the input with 'NoName 0'. Do not allow duplicate names to be read in. Terminate the input with an error message if duplicate names are read. At the end of the program, print all of the names to stdout, one per line (i.e. Inayah,25).*/
 
 
 #include "std_lib_facilities.h"
@@ -11,16 +11,16 @@ int main()
 {
     string user_name;
 
-    int user_age;
+    int user_score;
 
 	vector<string> names = {};
-	vector<int> ages = {};
+	vector<int> scores = {};
 
 	cout << "Enter a name and an age, i.e. Amber 23. Enter NoName 0 to exit.\n";
 
-	while(cin >> user_name >> user_age)
+	while(cin >> user_name >> user_score)
     {
-        if(user_name == "NoName" && user_age == 0)        
+        if(user_name == "NoName" && user_score == 0)        
             return 1;
         
         for(string x : names)
@@ -33,11 +33,11 @@ int main()
         }
 
         names.push_back(user_name);
-		ages.push_back(user_age);
+		scores.push_back(user_score);
 
 		for(int i = 0; i < names.size(); ++i)
         {
-			cout << names[i] << "," << ages[i] << '\n';
+			cout << names[i] << "," << scores[i] << '\n';
         }
     }
 
