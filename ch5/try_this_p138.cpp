13841023022fe5db930f4f1a449d8bf71fac1b61
Justin Noor <justin@justinnoor.io>
2018-04-28 07:56:17 -0700

    added try_this_p138.cpp

---
+++ ch5/try_this_p138.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+/*Compile the declarations below and see how the compiler responds*/
+
+
+int main()
+{
+    int s1 = area(7;
+    int s2 = area(7)
+    Int s3 = area(7);
+    int s4 = area('7);
+
+    return 0;
+}
