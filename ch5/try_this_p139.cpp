9a065d20d9d333df1db21ac81670a2ecfcf77fc3
Justin Noor <justin@justinnoor.io>
2018-04-28 20:19:58 -0700

    added try_this_p139.cpp

---
+++ ch5/try_this_p139.cpp
@@ -0,0 +1,22 @@
+//try_this_p139.cpp
+//=================
+/*Compile the declarations below and see how the compiler responds*/
+//==================================================================
+
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int area(int length, int width)
+{
+    return length * width;
+}
+
+int main()
+{
+    int x0 = arena(7);
+    int x1 = area(7);
+    int x2 = area("seven", 2);
+
+    return 0;
+}
