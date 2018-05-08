acc1f82a44019880868b35fb271bcf84dcb4259a
Justin Noor <justin@justinnoor.io>
2018-05-07 20:31:17 -0700

    added drill_167.cpp

---
+++ ch5/drill_p167.cpp
@@ -0,0 +1,49 @@
+//drill_p167.cpp
+//==============
+//Insert each of the 25 code fragments on pg.168 into the "scaffolding" program on pg.167 and see how the compiler reacts. If there are any errors in the fragments, fix them until the program compiles. DO NOT rewrite the "scaffolding" program 25 times. Insert a fragment, fix it, then remove it, then move on to the next.
+//======================================================================
+
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+try {
+      //Cout << "Success!\n";
+      //cout << "Success!\n";
+      //cout << "Success" << !\n"
+      //string res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+      //vector<int> v(10); v(5) = 7; if(v(5) != 7) cout << "Sucess!\n";
+      //if(cond) cout << "Success!\n"; else cout << "Fail!\n";
+      //bool c = false; if(c) cout << "Success!\n"; else cout << "Fail!\n";
+      //string s = "ape"; boo c = "fool" < s; if(c) cout << "Success!\n";
+      //string s = "ape"; if(s == "fool") cout << "Success!\n";
+      //string s = "ape"; if(s == "fool") cout < "Success!\n";
+      //string s = "ape"; if(s + "fool") cout < "Success!\n";
+      //vector<char> v(5); for(int i = 0; 0 < v.size(); ++i); cout << "Success!\n";
+      //vector<char> v(5); for(int i = 0; i < v.size(); ++i); cout << "Success!\n";
+      //string s = "Success!\n"; for(int i = 0; i < 6; ++i) cout << s[i];
+      //if(true) then cout << "Success!\n"; else cout << "Fail!\n";
+      //int x = 2000; char c = x; if(c == 2000) cout << "Success!\n";
+      //string s = "Success!\n"; for(int i = 0; i < 10; ++i) cout << s[i];
+      //vector v(5); for(int i = 0; i <= v.size(); ++i) cout << "Success!\n";
+      //int i = 0; int j = 9; while(i < 10) ++j; if(j < i) cout << "Success!\n";
+      //int x = 2; double d = 5 / (x - 2); if(d == 2 * x + 0.5) cout << "Success!\n";
+      //string<char> s = "Success!\n"; for(int i = 0; i <= 10; ++i) cout << s[i];
+      //int i = 0; while(i < 10) ++j; if(j < i) cout << "Success!\n";
+      //int x = 4; double d = 5 / (x - 2); if(d = 2 * x + 0.5) cout << "Success!\n";
+      //cin << "Success!\n";
+
+      keep_window_open();
+      return 0;
+}
+catch(exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch(...) {
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2;
+}
