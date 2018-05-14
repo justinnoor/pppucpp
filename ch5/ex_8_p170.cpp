ec1cd88ab0bd2ea7dd95b1e4b0a482a460d43177
Justin Noor <justin@justinnoor.io>
2018-05-14 16:28:10 -0700

    fixed typo

--- ch5/ex_8_p170.cpp
+++ ch5/ex_8_p170.cpp
@@ -25,7 +25,7 @@ int main()
     }
 
     for(int i = 0; i < n; ++i) {
-        sum += values[i]
+        sum += values[i];
     }
 
     cout << "The sum of the first " << n << " numbers is: " << sum << '\n';
